6e1a3c84511c11a78c1a5388bc56e64f4dfd19fa
Simon Aquino <simonaquino@gmail.com>
2020-12-09 00:52:20 +0100

    Removing unneeded var

--- vars.tf
+++ vars.tf
@@ -6,7 +6,3 @@ variable "region" {
   description = "Region in GCP"
   default = "europe-west4"
 }
-
-variable "terraform_state_bucket" {
-  description = "The bucket name holding the terraform state"
-}
