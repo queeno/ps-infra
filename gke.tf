8fe1a703e2482620804619ac37ade98a2bf70e3e
Simon Aquino <simonaquino@gmail.com>
2020-12-09 01:02:30 +0100

    Fix outstanding issues

--- gke.tf
+++ gke.tf
@@ -1,11 +1,12 @@
 data "google_client_config" "current" {}
 
 resource "google_container_cluster" "gke_cluster" {
-  name = "paymentsense"
+  provider = google-beta
+  name     = "paymentsense"
   location = "${var.region}-a"
-  network = google_compute_network.vpc_network.name
+  network  = google_compute_network.vpc_network.name
 
-  initial_node_count = "1"
+  initial_node_count       = "1"
   remove_default_node_pool = true
 
   networking_mode = "VPC_NATIVE"
@@ -20,12 +21,12 @@ resource "google_container_cluster" "gke_cluster" {
   private_cluster_config {
     enable_private_endpoint = false
     enable_private_nodes    = true
-    master_ipv4_cidr_block = "10.172.0.0/28"
+    master_ipv4_cidr_block  = "10.172.0.0/28"
   }
 
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block = "0.0.0.0/0"
+      cidr_block   = "0.0.0.0/0"
       display_name = "open to everywhere"
     }
   }
@@ -42,13 +43,13 @@ resource "google_container_cluster" "gke_cluster" {
 }
 
 resource "google_container_node_pool" "preemptible_node_pool" {
-  name = "preemptible"
-  cluster = google_container_cluster.gke_cluster.name
+  name       = "preemptible"
+  cluster    = google_container_cluster.gke_cluster.name
   node_count = "1"
 
   autoscaling {
-    max_node_count = 0
-    min_node_count = 2
+    max_node_count = 2
+    min_node_count = 0
   }
 
   location = "${var.region}-a"
@@ -56,6 +57,6 @@ resource "google_container_node_pool" "preemptible_node_pool" {
   node_config {
     machine_type = "n1-standard-2"
     disk_size_gb = "20"
-    preemptible = "true"
+    preemptible  = "true"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
-provider "google" {
+provider "google-beta" {
   project = var.project_id
 }
 
 provider "helm" {
   kubernetes {
-    load_config_file = false
-    host  = "https://${google_container_cluster.gke_cluster.endpoint}"
-    token = data.google_client_config.current.access_token
+    load_config_file       = false
+    host                   = "https://${google_container_cluster.gke_cluster.endpoint}"
+    token                  = data.google_client_config.current.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
 
 terraform {
   backend "gcs" {
-    prefix  = "terraform/state"
+    prefix = "terraform/state"
   }
 }
